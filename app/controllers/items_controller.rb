19ad31f3dd7a75ad1972a66a5d2777264e8e0937
uki-88 <uki88.nn.1227@gmail.com>
2021-03-26 19:29:39 +0900

    商品情報の登録機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, only: [:new]
   
+  def index
+
+  end
+
+  def new
+    @item  = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :shipping_area_id, :shipping_charge_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
