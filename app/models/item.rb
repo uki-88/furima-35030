7bb7865998a29a3fc8e76ba28b64822cbe40306a
uki-88 <uki88.nn.1227@gmail.com>
2021-03-26 16:48:21 +0900

    ActiveHashの実装、設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :category_id
-    validates :status_id
-    validates :shipping_charge_id
-    validates :shipping_area_id
-    validates :days_to_ship_id
     validates :price,              format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :image
   end
@@ -15,5 +10,20 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  with_options presence: true, numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+  end
 
 end
