b656c48c448984f359ddf0dd284897046eaffef4
uki-88 <uki88.nn.1227@gmail.com>
2021-03-26 12:51:07 +0900

    itemモデルにバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+    validates :price,              format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :image
+  end
+
+
   belongs_to :user
   has_one_attached :image
 
