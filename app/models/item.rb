9f1071c5ca4f00bbde504803afce0caab171647f
uki-88 <uki88.nn.1227@gmail.com>
2021-03-27 17:55:59 +0900

    Rubocop Part.2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,11 @@
 class Item < ApplicationRecord
-
   with_options presence: true do
     validates :name
     validates :text
-    validates :price,              format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :price, format: { with: /\A[0-9]+\z/ },
+                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
-
 
   belongs_to :user
   has_one_attached :image
@@ -18,12 +17,11 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :days_to_ship
 
-  with_options presence: true, numericality: { other_than: 1 }  do
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :shipping_charge_id
     validates :shipping_area_id
     validates :days_to_ship_id
   end
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name                {Faker::Team.name}
-    text                {Faker::Lorem.sentence}
-    category_id         {2}
-    status_id           {2}
-    shipping_area_id    {2}
-    shipping_charge_id  {2}
-    days_to_ship_id     {2}
-    price               {1000}
+    name                { Faker::Team.name }
+    text                { Faker::Lorem.sentence }
+    category_id         { 2 }
+    status_id           { 2 }
+    shipping_area_id    { 2 }
+    shipping_charge_id  { 2 }
+    days_to_ship_id     { 2 }
+    price               { 1000 }
     association :user
 
     after(:build) do |item|
