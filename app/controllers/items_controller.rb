95c97de567ee632fe7d23569bc0dfbca18965a0c
uki-88 <uki88.nn.1227@gmail.com>
2021-03-28 21:33:53 +0900

    商品情報編集機能の修正Part2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :find_params, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
