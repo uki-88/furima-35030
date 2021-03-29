f33578c8743bcfbaea95e8fca1593dd2f2b65ae9
uki-88 <uki88.nn.1227@gmail.com>
2021-03-29 19:22:20 +0900

    purchase,addressテーブルにカラム追加

--- db/migrate/20210329070851_create_addresses.rb
+++ db/migrate/20210329070851_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.string     :post_code,      null: false
+      t.integer    :shipping_area_id,  null: false
+      t.string     :municipality,     null: false
+      t.string     :address
+      t.string     :building
+      t.string     :telephone,        null: false
+      t.references :purchase,         null: false, foreign_key: true
 
       t.timestamps
     end
