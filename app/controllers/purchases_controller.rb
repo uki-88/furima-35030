50de1b481ce25ada17d57653bd4f82ac5d4cefa9
uki-88 <uki88.nn.1227@gmail.com>
2021-03-29 21:24:22 +0900

    purchasesコントローラーにメソッド定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,24 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def purchase_params
+  params.require(:purchase_address).permit(:post_code,:shipping_area_id, :municipality, :address, :building, :telephone, :item_id).merge(user_id: current_user.id)
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items
-  resources :purchases, only: [:new, :create]
+  resources :purchases, only: [:index, :create]
 end
