a9d094d8d3c06f581a29cfa44f6ffa2d169c68d0
uki-88 <uki88.nn.1227@gmail.com>
2021-03-29 20:38:19 +0900

    Formオブジェクトのモデル設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,21 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code,:shipping_area_id, :municipality, :address, :building, :telephone, :
+  attr_accessor :post_code,:shipping_area_id, :municipality, :address, :building, :telephone, :user_id, :item_id
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :shipping_area_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :municipality
+    validates :address
+    validates :telephone, format: {with: /\A\d{11}\z/}
+  end
+  
+
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building: building, telephone: telephone, purchase_id: purchase.id)
   end
 end
